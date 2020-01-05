e17e4cf660e137437e792d0b0065990b2dee0c38
Ana Mangas <anamangas@gmail.com>
2020-01-05 16:40:26 +0100

    Añadido Trace

--- service/src/main/java/iteriam/test4/calculadora/service/CalculadoraServiceImpl.java
+++ service/src/main/java/iteriam/test4/calculadora/service/CalculadoraServiceImpl.java
@@ -1,5 +1,6 @@
 package iteriam.test4.calculadora.service;
 
+import io.corp.calculator.TracerImpl;
 import iteriam.test4.calculadora.dto.EntradaDTO;
 import iteriam.test4.calculadora.dto.Operacion;
 import iteriam.test4.calculadora.dto.ResultadoDTO;
@@ -8,10 +9,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class CalculadoraServiceImpl implements CalculadoraService {
 
+	TracerImpl trazador = new TracerImpl();
 
 	@Override
 	public ResultadoDTO operacionBasica(Operacion operacion, EntradaDTO entrada) {
 		ResultadoDTO result = new ResultadoDTO(operacion, entrada);
+
+		trazador.trace(operacion);
+		trazador.trace(entrada);
+		trazador.trace(result);
 
 		return result;
 	}
